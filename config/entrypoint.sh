7d9f5083594e30ecdb508df71f97fad1d2ce01e5
Aaron Fagan <aaron@thefagans.ca>
2019-02-28 10:21:22 -0700

    cleanup filename generation

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -24,8 +24,8 @@ echo "DDNS is running!"
 
 for DOMAIN in $(echo $R53_DOMAINS | sed "s/,/ /g")
 do
-FILENAME="${DOMAIN//./_}"
-cat <<EOF >/etc/cron.d/ddns_r53_$FILENAME
+FILENAME="ddns_r53_${DOMAIN//./_}"
+cat <<EOF >/etc/cron.d/$FILENAME
 $CRON root /root/ddns-r53.sh --zone $R53_ZONE_ID --domain $DOMAIN --ttl $R53_TTL --ns $R53_NAME_SERVER > /proc/1/fd/1
 EOF
 bash /root/ddns-r53.sh --zone $R53_ZONE_ID --domain $DOMAIN --ttl $R53_TTL --ns $R53_NAME_SERVER > /proc/1/fd/1
