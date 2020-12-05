53ae311f1a2fd4f3250d7774729f6240274b8830
Aaron Fagan <aaron.fagan@telus.com>
2020-12-05 13:47:58 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,8 @@ dpkg-reconfigure -f noninteractive tzdata > /dev/null 2>&1
 
 [ "${AWS_ACCESS_KEY_ID}" ] && aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 [ "${AWS_SECRET_ACCESS_KEY}" ] && aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure set default.region ${AWS_DEFAULT_REGION}
-aws configure set default.output json
+aws configure set region ${AWS_DEFAULT_REGION}
+aws configure set output json
 
 R53_ZONE=$(echo ${R53_ZONE} | tr a-z A-Z)
 R53_TYPE=$(echo ${R53_TYPE} | tr a-z A-Z)
